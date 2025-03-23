641282052bb4ffdffac55d7b2357bd6fd1e730d6
Renu Yadav <renuya886@gmail.com>
2025-03-23 08:05:43 +0530

    hey

--- src/Components/Layout2.jsx
+++ src/Components/Layout2.jsx
@@ -10,12 +10,23 @@ import Skilltree from './Skilltree'
 const Layout2 = () => {
   return (
     <div className='p-3'>
-        <Head/>
-        <About />
-        <SkillsIcons/>
-        <Projects />
-        <Skilltree/>
-        <Experience/> 
+      <Head />
+      <About />
+      <SkillsIcons />
+      <Projects />
+      <Skilltree />
+      <Experience />
+      {/* footer */}
+      <motion.div
+        className='mt-80 font-serif text-xs font-bold text-gray-600'
+        initial={{ y: 200, opacity: 0 }}
+        whileInView={{ y: -100, opacity: 1 }}
+        transition={{ type: "tween", duration: 1, ease: "easeOut" }}
+      >
+
+        <p>Thank's for reaching the bottom of this page. If you like what you see, let's connect and build something together.</p>
+        <a className='text-blue-400 hover:underline' href="public\RESUMEYADAV (1).pdf">resume</a>
+      </motion.div>
     </div>
   )
 }
